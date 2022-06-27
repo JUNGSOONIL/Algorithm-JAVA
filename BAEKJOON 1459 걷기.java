6306f68f799261ba7c030414a42550e3ac69a99a
순일 <88392868+JUNGSOONIL@users.noreply.github.com>
2022-06-27 11:19:34 +0900

    Update BAEKJOON 1459 걷기.java

--- BAEKJOON 1459 걷기.java
+++ BAEKJOON 1459 걷기.java
@@ -1,10 +1,8 @@
-package BJ;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BJ1459걷기 {
+public class Main {
 
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
